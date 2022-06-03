c17fca144387319caf7719e0d8abd34fd20fc8a7
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-06-03 17:53:20 +0900

    add getDiagnoticsTest

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,15 +1,14 @@
 import fetch, { BodyInit } from 'node-fetch';
 import FormData = require('form-data');
 
-export async function api<T>(url: string, encodedDocumentText: string): Promise<T> {
+export async function postFile<T>(url: string, documentName: string, encodedDocumentText: string): Promise<T> {
 	const body = new FormData();
 	// TODO
 	// 1. 파일 타입 받아오기
 	// 2. 파일 이름 받아오기
-	body.append("type", "text/plain"); // 이건 옵션
-	body.append("name", "TEST.txt");
+	body.append("name", documentName);
 	body.append("body", encodedDocumentText);
-	console.log("api called");
+	console.log("postFile called");
 	return fetch(
 		url,
 		{
@@ -17,6 +16,16 @@ export async function api<T>(url: string, encodedDocumentText: string): Promise<
 			body: body as BodyInit
 		}
 	).then(response => {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		return response.json() as Promise<T>;
+	});
+}
+
+export async function getDiagnoticsTest<T>(url: string): Promise<T> {
+	console.log("getDiagnoticsTest called");
+	return fetch(url).then(response => {
 		if (!response.ok) {
 			throw new Error(response.statusText);
 		}

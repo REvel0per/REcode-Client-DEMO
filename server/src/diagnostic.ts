7a429772a0ee6ba544a2cfb8c8c9b3d221b427bc
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-06-05 01:40:51 +0900

    feat: more rich visualization

--- server/src/diagnostic.ts
+++ server/src/diagnostic.ts
@@ -1,25 +1,29 @@
+import { assert } from 'console';
 import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver';
-import { DiagnosticsRes } from './interface';
+import { BugInterface, DiagnosticsRes } from './interface';
 
 export function parseDiagnostic(responce: DiagnosticsRes): Diagnostic[] {
 	const diagnostics: Diagnostic[] = [];
 	const files = responce.files;
 	files.forEach(file => {
 		const name: string = file.filename;
-		const bugs = file.bug;
-		bugs.forEach(bug => {
+		const bugs = JSON.parse(file.bug);
+		console.log(bugs);
+		bugs.forEach((bug: BugInterface) => {
+			const varName: string = bug.qualifier.split('`')[1];
+			const varLength = varName ? varName.length : 0;
 			const diagnostic: Diagnostic = {
 				range: {
 					start: { 
-						line: bug.line,
+						line: bug.line-1,
 						character: bug.column
 					},
 					end: { 
-						line: bug.line,
-						character: bug.column+1
+						line: bug.line-1,
+						character: bug.column+varLength
 					}
 				},
-				message: `[${bug.bug_type}]` + bug.qualifier
+				message: `[${bug.bug_type_hum}] ` + bug.qualifier
 			};
 			if (bug.severity === 'ERROR') {
 				diagnostic.severity = DiagnosticSeverity.Error;

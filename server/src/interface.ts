25d871d0e7e2879dfff16390f605e802faef11c9
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-06-07 00:25:35 +0900

    add TraceInterface

--- server/src/interface.ts
+++ server/src/interface.ts
@@ -1,17 +1,43 @@
+export interface TraceInterface {
+	level: number
+  filename: string
+  line_number: number
+  column_number: number
+  description: string
+}
+
 export interface BugInterface {
 	bug_type: string
 	qualifier: string
 	severity: string
 	line: number
 	column: number
+	bug_trace: TraceInterface[]
 	procedure: string
   procedure_start_line: number
 	bug_type_hum: string
 }
 
+export interface LintInterface {
+	type: string
+	line: string
+	column: string
+	message: string
+}
+
+export interface FileLintInterface {
+	filename: string
+	lint: LintInterface[]
+}
+
 export interface FileInterface {
 	filename: string
-	bug: string
+	bug: BugInterface[]
+	lint: LintInterface[]
+}
+
+export interface LintRes {
+	files: FileLintInterface[]
 }
 
 export interface DiagnosticsRes {
